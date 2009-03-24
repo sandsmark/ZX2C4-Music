c2afdc538c24d62b3676689b136e95165d6b9d8d
Jason A. Donenfeld <Jason@zx2c4.com>
2009-03-24 11:22:28 -0400

    Removed track and file from log info.

--- logger.php
+++ logger.php
@@ -4,15 +4,13 @@ function setupLogDatabase()
 {
 	mysql_query(	"CREATE TABLE IF NOT EXISTS requestlog (
 			id INT NOT NULL AUTO_INCREMENT,
-			leaderid INT,
 			PRIMARY KEY(id),
+			leaderid INT,
 			time INT NOT NULL,
 			ip VARCHAR(30),
 			useragent VARCHAR(255),
 			zip BOOL,
 			sha1 VARCHAR(64),
-			file VARCHAR(255),
-			track INT,
 			artist VARCHAR(255),
 			album VARCHAR(255),
 			title VARCHAR(255)
@@ -28,18 +26,17 @@ function logDownload($songArray, $zip)
 	$first = -1;
 	foreach($songArray as $song)
 	{
-		mysql_query("INSERT INTO requestlog (leaderid, time, ip, useragent, zip, sha1, file, track, artist, album, title) VALUES (
+		mysql_query("INSERT INTO requestlog (leaderid, time, ip, useragent, zip, sha1, artist, album, title) VALUES (
 			".$first.",
 			".time().",
 			".nullString($_SERVER["REMOTE_ADDR"]).",
 			".nullString($_SERVER["HTTP_USER_AGENT"]).",
 			".sqlBool($zip).",
 			".nullString($song["sha1"]).",
-			".nullString($song["file"]).",
-			".nullInt($song["track"]).",
 			".nullString($song["artist"]).",
 			".nullString($song["album"]).",
-			".nullString($song["title"]).");");
+			".nullString($song["title"]).");
+		");
 		if($first == -1)
 		{
 			$first = mysql_insert_id();
