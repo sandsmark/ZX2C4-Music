3fd1d9b66367beb412124191ed7a331a3557d6e4
Jason A. Donenfeld <Jason@zx2c4.com>
2009-04-29 23:45:15 -0400

    Added utf-8 support for search box.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php header("Content-Type: text/html; charset=UTF-8"); ?>
 <?php require_once("authenticate.php"); ?>
 <?php if(!eregi("MSIE", $_SERVER['HTTP_USER_AGENT']) && !eregi("Internet Explorer", $_SERVER['HTTP_USER_AGENT'])) { ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
