4e1a1c5f56e415a01b46eababc2bfb5d425b4fde
Jason A. Donenfeld <Jason@zx2c4.com>
2010-02-03 00:46:25 -0500

    Copyright.

--- login.php
+++ login.php
@@ -29,7 +29,7 @@
 </form>
 </p>
 <br>
-<span id="copyright">ZX2C4 Music is &copy; Copyright 2008-2009 Jason A. Donenfeld. All Rights Reserved.</span>
+<span id="copyright">ZX2C4 Music is &copy; Copyright 2008-2010 Jason A. Donenfeld. All Rights Reserved.</span>
 </div>
 </body>
 </html>
